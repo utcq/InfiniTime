19f8909b279ecaf9d42950ee7ed86eb6981b9c74
utcq <utcq@waifu.club>
2024-10-14 21:20:47 +0200

    First topics

--- src/displayapp/screens/Cheat.cpp
+++ src/displayapp/screens/Cheat.cpp
@@ -5,18 +5,17 @@
 #include "components/motor/MotorController.h"
 #include "components/motion/MotionController.h"
 
+Topic topics[7] = {
+  {"Volume", VOLUME},
+  {"Temperatura", TEMPERATURA},
+  {"Pressione", PRESSIONE},
+  {"Densita'", DENSITA},
+  {"Decantazione", DECANTAZIONE},
+  {"Filtrazione", FILTRAZIONE},
+  {"Evaporazione", EVAPORAZIONE}
+};
+
 using namespace Pinetime::Applications::Screens;
-
-static const char* CHIMICA = R"(
-Decantazione,
-Filtrazione,
-Centrifugazione,
-Critallizzazione,
-Estrazione con solvente,
-Cromatografia,
-Distillazione
-)";
-
 namespace {
   lv_obj_t* MakeLabel(lv_font_t* font,
                       lv_color_t color,
@@ -41,17 +40,49 @@ namespace {
     return label;
   }
 
+  void topic_btn_handler(lv_obj_t *obj, lv_event_t event) {
+    if (event == LV_EVENT_CLICKED) {
+      for (int i=0;i<sizeof(topics)/sizeof(Topic);i++) {
+        if (topics[i].btn == obj) {
+          lv_obj_clean(lv_scr_act());
+          lv_scr_load_anim(topics[i].page, LV_SCR_LOAD_ANIM_FADE_ON, 300, 0, false);
+          break;
+        }
+      }
+    }
+  }
+}
+
+lv_obj_t *Cheat::create_page(const char *text) {
+  lv_obj_t *page = lv_obj_create(nullptr, nullptr);
+  lv_obj_t *ta = lv_textarea_create(page, nullptr);
+  lv_textarea_set_one_line(ta, false);
+  lv_textarea_set_text(ta, text);
+  lv_textarea_set_cursor_pos(ta, 0);
+  lv_obj_set_style_local_bg_color(page, LV_PAGE_PART_BG, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  lv_obj_set_style_local_bg_color(ta, LV_PAGE_PART_BG, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  lv_obj_set_style_local_text_color(ta, LV_PAGE_PART_BG, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+  lv_obj_set_size(ta, LV_HOR_RES, LV_VER_RES - 10);
+  return page;
 }
 
 Cheat::Cheat() {
-  this->ta = lv_textarea_create(lv_scr_act(), NULL);
-  lv_obj_set_size(ta, LV_HOR_RES-10, LV_VER_RES - 10);
-  lv_obj_set_pos(ta, 5, 5);
-  lv_textarea_set_text(ta, CHIMICA);
-  lv_textarea_set_cursor_pos(ta, 0);
-  lv_textarea_set_scrollbar_mode(ta, LV_SCROLLBAR_MODE_ON);
-  lv_textarea_set_edge_flash(ta, true);
-  
+  btnList = lv_list_create(lv_scr_act(), nullptr);
+  lv_obj_set_size(btnList, LV_HOR_RES, LV_VER_RES);
+  lv_obj_align(btnList, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_set_style_local_bg_color(btnList, LV_LIST_PART_BG, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  lv_obj_t * list_btn;
+  for (int i=0;i<sizeof(topics)/sizeof(Topic);i++) {
+    Topic *topic = &topics[i];
+    topic->page = create_page(topic->content);
+    list_btn = lv_list_add_btn(btnList, nullptr, topic->name);
+    lv_obj_set_style_local_bg_color(list_btn, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_ORANGE);
+    lv_obj_set_style_local_radius(list_btn, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, 10);
+    lv_obj_set_style_local_text_color(list_btn, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_style_local_margin_top(list_btn, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, 10);
+    topic->btn = list_btn;
+    lv_obj_set_event_cb(list_btn, topic_btn_handler);
+  }
   refreshTask = lv_task_create(RefreshTaskCallback, LV_DISP_DEF_REFR_PERIOD, LV_TASK_PRIO_MID, this);
 }
 
@@ -59,27 +90,9 @@ Cheat::Cheat() {
 Cheat::~Cheat() {
   lv_task_del(refreshTask);
   lv_obj_clean(lv_scr_act());
+  for (int i=0;i<sizeof(topics)/sizeof(Topic);i++) {
+    if (topics[i].page) lv_obj_clean(topics[i].page);
+  }
 }
 
-bool Cheat::OnTouchEvent(Pinetime::Applications::TouchEvents event) {
-  /* lv_textarea_set_cursor_pos(ta, ta_y);
-  if (event == Pinetime::Applications::TouchEvents::SwipeDown) {
-    ta_y += 10;
-  }
-  if (event == Pinetime::Applications::TouchEvents::SwipeUp) {
-    ta_y -= 10;
-    ta_y = ta_y < 0 ? 0 : ta_y;
-  }
-  return true; */
-  if (event == Pinetime::Applications::TouchEvents::SwipeDown) {
-    ta_y=0; setCursor = true;
-    return true;
-  }
-  return false;
-}
-
-void Cheat::Refresh() {
-  if (setCursor)
-    lv_textarea_set_cursor_pos(ta, ta_y);
-    setCursor = false;
-}
+void Cheat::Refresh() {}
--- src/displayapp/screens/Cheat.h
+++ src/displayapp/screens/Cheat.h
@@ -9,6 +9,42 @@
 #include <array>
 #include <random>
 
+struct Topic {
+  const char* name;
+  const char *content;
+  lv_obj_t *page = nullptr;
+  lv_obj_t *btn = nullptr;
+};
+
+static const char* VOLUME = R"(
+Il volume è una grandezza fisica che esprime lo spazio occupato da un corpo.
+)";
+
+static const char* TEMPERATURA = R"(
+La temperatura è una grandezza fisica che esprime il calore di un corpo.
+)";
+
+static const char* PRESSIONE = R"(
+La pressione è una grandezza fisica che esprime la forza esercitata su una superficie.
+)";
+
+static const char* DENSITA = R"(
+La densita' è una grandezza fisica che esprime la quantità di materia presente in un volume.
+)";
+
+static const char* DECANTAZIONE = R"(
+La decantazione
+)";
+
+static const char *FILTRAZIONE = R"(
+La filtrazione
+)";
+
+static const char *EVAPORAZIONE = R"(
+L'evaporazione
+)";
+
+
 namespace Pinetime {
   namespace Applications {
     namespace Screens {
@@ -17,13 +53,12 @@ namespace Pinetime {
         Cheat();
         ~Cheat() override;
         void Refresh() override;
-        bool OnTouchEvent(Pinetime::Applications::TouchEvents event) override;
 
       private:
         lv_task_t* refreshTask;
-        lv_obj_t *ta;
-        bool setCursor = false;
-        int ta_y = 0;
+        lv_obj_t *btnList;
+
+        lv_obj_t *create_page(const char *text);
       };
     }
 
